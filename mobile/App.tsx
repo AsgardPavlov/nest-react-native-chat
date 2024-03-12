ec370acd606317cd70ba1388f2651b85d834170d
AsgardPavlov <pasgard9@gmail.com>
2024-03-12 13:37:35 +0700

    add fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,33 @@
+import { useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 
+import { commonStyles } from "./src/styles/common";
+
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "FC-Subject-Rounded-Regular": require("./assets/fonts/FC-Subject-Rounded-Regular.ttf"),
+    "FC-Subject-Rounded-Bold": require("./assets/fonts/FC-Subject-Rounded-Bold.ttf")
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hey, there!</Text>
+    <View
+      style={styles.container}
+      onLayout={onLayoutRootView}
+    >
+      <Text style={[commonStyles.defaultFont]}>Hey, there!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ mobile/src/styles/common.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const commonStyles = StyleSheet.create({
+  defaultFont: {
+    fontFamily: "FC-Subject-Rounded-Regular"
+  },
+  defaultFontBold: {
+    fontFamily: "FC-Subject-Rounded-Bold"
+  }
+});
