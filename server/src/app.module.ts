377d49a65d710019b02fa525218f694bae619cb6
AsgardPavlov <pasgard9@gmail.com>
2024-03-14 04:10:41 +0700

    add postgres module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostgresModule } from './shared/modules/postgres.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), PostgresModule],
   controllers: [AppController],
   providers: [AppService],
 })
