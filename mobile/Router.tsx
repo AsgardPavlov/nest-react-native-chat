b4674be2bed075458ac8fb0de3a503b229d3976a
AsgardPavlov <pasgard9@gmail.com>
2024-03-14 23:10:35 +0700

    add mobile navigation

---
+++ mobile/Router.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from "./src/screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+const routes = {
+  Home: {
+    component: Home,
+    options: { headerShown: false }
+  }
+};
+
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {Object.entries(routes).map(([name, screen]) => (
+          <Stack.Screen
+            key={name}
+            name={name}
+            {...screen}
+          />
+        ))}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
