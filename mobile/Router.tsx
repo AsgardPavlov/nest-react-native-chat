f06cf6013b4f407841606c553497d4fc8412e5de
AsgardPavlov <pasgard9@gmail.com>
2024-03-16 05:26:33 +0700

    update header navigation UI

--- mobile/Router.tsx
+++ mobile/Router.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
+import { ChevronLeftIcon, PlusIcon } from "react-native-heroicons/outline";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import BottomNavigationBar from "./src/layout/BottomNavigationBar";
 import AddCard from "./src/screens/AddCard";
 import Cards from "./src/screens/Cards";
+import { Colors } from "./src/styles/colors";
+import { commonStyles } from "./src/styles/common";
 import { RootStackParamList } from "./src/types/screens";
+import { fontPixel } from "./src/utils/font-size-helper";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -26,7 +31,35 @@ const routes = {
 
 const Router = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={({ navigation, route }) => ({
+        headerLeft: ({ canGoBack }) =>
+          canGoBack ? (
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <ChevronLeftIcon
+                size={23}
+                color={Colors.BLACK}
+                strokeWidth={2.3}
+              />
+            </TouchableOpacity>
+          ) : null,
+        headerRight: () =>
+          route.name === "Cards" ? (
+            <TouchableOpacity onPress={() => navigation.navigate("AddCard")}>
+              <PlusIcon
+                size={23}
+                color={Colors.BLACK}
+                strokeWidth={2.3}
+              />
+            </TouchableOpacity>
+          ) : null,
+        headerTitleStyle: {
+          ...commonStyles.defaultFontBold,
+          fontSize: fontPixel(17)
+        },
+        headerShadowVisible: false
+      })}
+    >
       {Object.entries(routes).map(([name, screen]) => (
         <Stack.Screen
           key={name}
